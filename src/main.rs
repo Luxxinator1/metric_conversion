c0a9f180449bcff7c5427a44b28766e09536433a
Luxxinator1 <lukas.koenig.regensburg@protonmail.com>
2025-01-21 17:47:54 +0100

    Add version system with error handling

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
             .read_line(&mut metric_system)
             .expect("Failed to read the line!");
 
-        let _metric_system: i32 = match metric_system.trim().parse() {
+        let metric_system: i32 = match metric_system.trim().parse() {
             Ok(metric_system) => metric_system,
             Err(_) => {
                 println!("{metric_system} is not a number!");
@@ -27,12 +27,35 @@ fn main() {
             .read_line(&mut temperature)
             .expect("Please input a vaild charachter!");
 
-        let _temperature: f32 = match temperature.trim().parse() {
+        let temperature: f32 = match temperature.trim().parse() {
             Ok(temperature) => temperature,
             Err(_) => {
                 println!("{temperature} is not a number!");
                 continue;
             }
         };
+
+        let celsius: f32;
+
+        if metric_system == 2 {
+            // Celsius = (Fahrenheit - 32) / 1.8
+            celsius = (temperature - 32.0) / 1.8;
+            println!("{celsius}C");
+            break;
+        }
+
+        let fahrenheit: f32;
+
+        if metric_system == 1 {
+            // Fahrenheit = (Celsius * 1.8) + 32
+            fahrenheit = (temperature * 1.8) + 32.0;
+            println!("{fahrenheit}F");
+            break;
+        }
+
+        if metric_system != 1 && metric_system != 2 {
+            println!("Please input a valid charachter!");
+            continue;
+        }
     }
 }
