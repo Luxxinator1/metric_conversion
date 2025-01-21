2a515b35a9718556d55a36e26d5fa953fb6876e8
Luxxinator1 <lukas.koenig.regensburg@protonmail.com>
2025-01-21 17:45:43 +0100

    Add version system with error handling

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use std::io;
 
 fn main() {
     loop {
-        let mut metric_system = String::new();
-
         println!("Celsius => Fahrenheit (input 1)");
         println!("Fahrenheit => Celsius (input 2)");
+
+        let mut metric_system = String::new();
 
         io::stdin()
             .read_line(&mut metric_system)
@@ -13,11 +13,15 @@ fn main() {
 
         let _metric_system: i32 = match metric_system.trim().parse() {
             Ok(metric_system) => metric_system,
-            Err(_) => continue,
+            Err(_) => {
+                println!("{metric_system} is not a number!");
+                continue;
+            }
         };
 
-        let mut temperature = String::new();
         println!("Input your temperature: ");
+
+        let mut temperature = String::new();
 
         io::stdin()
             .read_line(&mut temperature)
@@ -25,7 +29,10 @@ fn main() {
 
         let _temperature: f32 = match temperature.trim().parse() {
             Ok(temperature) => temperature,
-            Err(_) => continue,
+            Err(_) => {
+                println!("{temperature} is not a number!");
+                continue;
+            }
         };
     }
 }
